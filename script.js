43872fad5c248cd92b57de5ccb4991ac88139b3c
Vishal Anand <66544968+Spiritman021@users.noreply.github.com>
2021-07-18 18:36:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+$(document).ready(function () {
+
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function () {
+
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if ($(window).scrollTop() > 0) {
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+
+    });
+
+    // smooth scrolling 
+
+    $('a[href*="#"]').on('click', function (e) {
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop: $($(this).attr('href')).offset().top,
+
+        },
+            500,
+            'linear'
+        );
+
+    });
+
+});
